dd049590e26f4d4866818ca7409d39a97d12d878
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-08-30 13:38:27 -0500

    user and thought models

---
+++ models/Thought.js
@@ -0,0 +1,59 @@
+const { Schema, model, Types } = require("mongoose");
+
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      //maximum 280 chars
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      //must be between 1-280 chars
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)// need to create a date format utils
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model, Types } = require("mongoose");
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    //must match valid email
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+    //match taken from stack overflow
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},
+{
+  toJSON: {
+    virtuals: true
+  }
+});
+
+//get count of friends for the user
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// create User model using UserSchema
+const User = model("User",UserSchema);
+
+
+module.exports = User;
